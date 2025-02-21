cf6b33311c14fd446873b5a5cfdc19988425be4c
Siddhesh <siddheshgite2004@gmail.com>
2025-02-21 17:55:34 +0530

    first commit

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,15 @@
+import StartupForm from "@/components/StartupForm"
+
+const page = () => {
+  return (
+    <>
+  <section className="pink_container !min-h-[230px]">
+    <h1 className="heading">Submit Your Startup</h1>
+    <h1 className="mt-3"></h1>
+  </section>
+   <StartupForm/>
+    </>
+  )
+}
+
+export default page
